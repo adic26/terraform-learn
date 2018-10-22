1a1bd22cc3be98c30d07430ea5fc17aa6899a681
Aditya Chugh <chugh.aditya@gmail.com>
2018-10-22 16:40:24 -0400

    Configure remote state

--- providers.tf
+++ providers.tf
@@ -1,3 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "${var.s3_region}"
+}
+
+terraform {
+  required_version = ">= 0.11.9"
+
+  backend "s3" {
+    bucket = "cpweb-tf-state-backend"
+    key = "test/backbone"
+    region = "us-east-1"
+    encrypt = "true"
+  }
 }
