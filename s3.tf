79c9f355f82d5a39d2410f146f4bc2f1785c146b
Aditya Chugh <chugh.aditya@gmail.com>
2018-10-21 23:59:00 -0400

    Adding variables

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "test" {
-	bucket = "cpweb-tf-first-bucket"
+	bucket = "${var.s3_bucket_name}"
 	acl = "private"
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "s3_bucket_name" {
+  type = "string"
+  description = "Name of S3 Bucket"
+}
+
+variable "s3_region" {
+  type = "string"
+  default = "us-east-1"
+}
